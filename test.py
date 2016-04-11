ea0e851e3c9d43ca027f02c0a809cbe0352f34be
Olivia Guest <guest.olivia@gmail.com>
2016-04-11 21:46:09 +0100

    Loading images and running PCA

---
+++ test.py
@@ -0,0 +1,33 @@
+from scipy import misc
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from sklearn.decomposition import PCA
+import glob, os
+
+path = 'img/'
+img_array = []
+os.chdir(path)
+img_array = np.zeros((37, 10000))
+for i, img_file in enumerate(glob.glob("*.jpg")):
+    print(img_file)
+    img = Image.open(img_file).convert('L')
+   
+    #img = Image.open(img_file).convert('LA')
+
+    #plt.imshow(img)
+    #plt.show()
+    img = Image.open(img_file).convert('L')
+    img = img.resize((100, 100), Image.ANTIALIAS)
+
+    #img_array.append(img.getdata())
+    img_array[i] = img.getdata()
+
+    
+#print type(img_array[0])
+#print type(img_array)
+
+pca = PCA(n_components=2)
+pca.fit(img_array)
+PCA(copy=True, n_components=2, whiten=False)
+print(pca.explained_variance_ratio_) 
