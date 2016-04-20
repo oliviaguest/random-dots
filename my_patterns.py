3e6e34d0f1d485c8b35f06412df765952e6f8350
Olivia Guest <guest.olivia@gmail.com>
2016-04-20 13:51:21 +0100

    Adding optional save/load examples

--- my_patterns.py
+++ my_patterns.py
@@ -3,3 +3,5 @@
 from randomdots import *
 p = Patterns(categories = 3, items_per_category = [10, 8, 4, 1], include_prototypes = False)
 p.Dendrograms()
+#p.Save('my_patterns.pkl') #uncomment to save
+#p.Load('my_patterns.pkl') #uncomment to load
